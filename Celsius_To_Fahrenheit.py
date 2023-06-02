3c9aa08be9bcc6e4d47fccde5948e1502a6dc78d
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2023-06-02 14:09:10 +0530

    celsius_temp = int(input()) # converting celsius degree temperature to Fahrenheit fahrenheit_temp =celsius_temp*1.8+32 # printing the Fahrenheit equivalent of the given input celsius degree print("%.2f"%fahrenheit_temp)

---
+++ Celsius_To_Fahrenheit.py
@@ -0,0 +1,5 @@
+celsius_temp = int(input())
+# converting celsius degree temperature to Fahrenheit
+fahrenheit_temp =celsius_temp*1.8+32
+# printing the Fahrenheit equivalent of the given input celsius degree
+print("%.2f"%fahrenheit_temp)
